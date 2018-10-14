d5a74558f52bac9b9378e2a5f1353964a0e49d11
Robert Chen <robertchen@live.com>
2018-10-14 10:12:18 -0700

    Installed and ran prettier

--- src/app.js
+++ src/app.js
@@ -2,28 +2,33 @@ const PORT = process.env.PORT || 3000;
 
 const compression = require("compression");
 const iconv = require("iconv-lite");
-const express = require('express');
-const request = require("request").defaults({jar: true});
+const express = require("express");
+const request = require("request").defaults({ jar: true });
 
 const fs = require("fs");
 
 const app = express();
-const concat = require('concat-stream');
-app.use(function(req, res, next){
-  req.pipe(concat(function(data){
-    req.body = data;
-    next();
-  }));
+const concat = require("concat-stream");
+app.use(function(req, res, next) {
+  req.pipe(
+    concat(function(data) {
+      req.body = data;
+      next();
+    })
+  );
 });
 app.use(compression());
-const http = require('http').Server(app);
+const http = require("http").Server(app);
 
 http.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 app.use((req, res, next) => {
   // Don't redirect in development
-  if (process.env.NODE_ENV === 'production' && req.header('x-forwarded-proto') !== 'https') {
-    res.redirect(`https://${req.header('host') + req.url}`);
+  if (
+    process.env.NODE_ENV === "production" &&
+    req.header("x-forwarded-proto") !== "https"
+  ) {
+    res.redirect(`https://${req.header("host") + req.url}`);
   } else {
     next();
   }
@@ -34,71 +39,83 @@ app.get("/", (req, res) => {
 });
 
 app.get("/*", (req, res) => {
-  let url = (req.originalUrl).substring("/".length);
-  
+  let url = req.originalUrl.substring("/".length);
+
   // Hack for stuff like http://localhost:3000/https:/reddit.com/map
-  if(url.indexOf("//") === -1){
+  if (url.indexOf("//") === -1) {
     url = url.replace("/", "//");
   }
-  
-  if(!url.startsWith("https://") && !url.startsWith("http://")){
+
+  if (!url.startsWith("https://") && !url.startsWith("http://")) {
     url = "/https://" + url;
     return res.redirect(url);
   }
-  
-  console.log(`GET ${url}`)
-  
+
+  console.log(`GET ${url}`);
+
   const userAgent = req.headers["user-agent"];
   const headers = {
     "User-Agent": userAgent
-  }
-  request({url, headers, encoding: null}, (error, response, body) => {
+  };
+  request({ url, headers, encoding: null }, (error, response, body) => {
     if (error) {
       res.send(`/*\nInvalid url ${url}\n*/`);
       return;
     }
-    if(response.request.uri.href !== url){
+    if (response.request.uri.href !== url) {
       return res.redirect("/" + response.request.uri.href);
     }
-    
-    if(response.headers["content-encoding"]) res.setHeader("Content-Encoding", response.headers["content-encoding"]);
-    
-    if (response.headers['content-type'] !== undefined) {
-      res.setHeader("Content-Type", response.headers['content-type']);
-      
-      if(response.headers["content-type"].indexOf("text/html") > -1){
-        const contentRegex = /(?<=charset=)[^\s]*/i
-        
+
+    if (response.headers["content-encoding"])
+      res.setHeader("Content-Encoding", response.headers["content-encoding"]);
+
+    if (response.headers["content-type"] !== undefined) {
+      res.setHeader("Content-Type", response.headers["content-type"]);
+
+      if (response.headers["content-type"].indexOf("text/html") > -1) {
+        const contentRegex = /(?<=charset=)[^\s]*/i;
+
         let regexMatches = contentRegex.exec(response.headers["content-type"]);
-        if(regexMatches == null) regexMatches = ["utf-8"];
-        
+        if (regexMatches == null) regexMatches = ["utf-8"];
+
         let htmlContent = iconv.decode(body, regexMatches[0]);
-        
+
         const headRegex = /<head.*?>/i;
         const bodyRegex = /<body.*?>/i;
         const headMatch = headRegex.exec(htmlContent);
         const bodyMatch = bodyRegex.exec(htmlContent);
-        
-        const headIndex = headMatch? headMatch.index + headMatch[0].length: 0;
-        const bodyIndex = bodyMatch? bodyMatch.index + bodyMatch[0].length: 0;
-        const injectedScript = `\n<script data-used="true">${fs.readFileSync(__dirname + "/inject.js", "utf8")}</script>\n`;
-        const injectedHeader = `\n${fs.readFileSync(__dirname + "/loading.html", "utf8")}\n`;
-        
-        const newHtml = htmlContent.substring(0, headIndex) + injectedScript + htmlContent.substring(headIndex, bodyIndex) + injectedHeader + htmlContent.substring(bodyIndex);
+
+        const headIndex = headMatch ? headMatch.index + headMatch[0].length : 0;
+        const bodyIndex = bodyMatch ? bodyMatch.index + bodyMatch[0].length : 0;
+        const injectedScript = `\n<script data-used="true">${fs.readFileSync(
+          __dirname + "/inject.js",
+          "utf8"
+        )}</script>\n`;
+        const injectedHeader = `\n${fs.readFileSync(
+          __dirname + "/loading.html",
+          "utf8"
+        )}\n`;
+
+        const newHtml =
+          htmlContent.substring(0, headIndex) +
+          injectedScript +
+          htmlContent.substring(headIndex, bodyIndex) +
+          injectedHeader +
+          htmlContent.substring(bodyIndex);
         return res.send(newHtml);
       }
     }
     res.send(new Buffer(body));
   });
-}); 
+});
 
 app.post("/*", (req, res) => {
-  const url = (req.originalUrl).substring("/".length);
-  
+  const url = req.originalUrl.substring("/".length);
+
   console.log(`POST ${url}`);
-  
-  const userAgent = req.headers["user-agent"];  
-  const contentType = req.headers['content-type'];
+
+  const userAgent = req.headers["user-agent"];
+  const contentType = req.headers["content-type"];
   const headers = {
     "Content-Type": contentType,
     "User-Agent": userAgent
@@ -108,13 +125,13 @@ app.post("/*", (req, res) => {
     body: req.body,
     headers,
     url: url
-  }
+  };
   request(options, (err, response, body) => {
     if (err) {
       res.send("Invalid url " + url);
       return;
     }
-    
+
     res.send(body);
   });
 });
--- src/inject.js
+++ src/inject.js
@@ -1,103 +1,129 @@
-if(!window.injectedScriptRunOnce){
+if (!window.injectedScriptRunOnce) {
   window.injectedScriptRunOnce = ":)";
-  (function(){
+  (function() {
     const hostNameRegex = /(?<=\/)http(s)?:\/\/.*?(?=\/)/i;
     let searchStr = window.location.href;
-    if(searchStr.charAt(searchStr.length - 1) != "/")searchStr += "/";
+    if (searchStr.charAt(searchStr.length - 1) != "/") searchStr += "/";
 
     // Includes protocol (https://)
-    const hostName = (hostNameRegex.exec(searchStr)[0]);
-    const protocol = window.location.href.indexOf("http://") == -1? "https://": "http://";
+    const hostName = hostNameRegex.exec(searchStr)[0];
+    const protocol =
+      window.location.href.indexOf("http://") == -1 ? "https://" : "http://";
 
-    const cleanUrl = function(url){
+    const cleanUrl = function(url) {
       const originalUrl = url;
-      
+
       // If data url, don't proxy
-      if(url.startsWith("data:")) return url;
-      
+      if (url.startsWith("data:")) return url;
+
       // Urls of form `//website.com/resource`
-      if(url.startsWith("//")){
-        return (protocol + window.location.host + "/" + (hostName.includes("https")? "https://": "http://") + url.substring(2));
+      if (url.startsWith("//")) {
+        return (
+          protocol +
+          window.location.host +
+          "/" +
+          (hostName.includes("https") ? "https://" : "http://") +
+          url.substring(2)
+        );
       }
       // Urls of form `index.js` (note NOT `/index.js`)
-      if(!url.includes("/")){
+      if (!url.includes("/")) {
         const loc = window.location.pathname;
-        const dir = loc.substring(0, loc.lastIndexOf('/'));
-        
+        const dir = loc.substring(0, loc.lastIndexOf("/"));
+
         return protocol + window.location.host + dir + "/" + url;
       }
-      
-      if(url.startsWith(window.location.origin)){
+
+      if (url.startsWith(window.location.origin)) {
         url = url.substring(window.location.origin.length);
-        if(url.charAt(0) === "/")url = url.substring(1);
+        if (url.charAt(0) === "/") url = url.substring(1);
       }
-      
-      
+
       // Urls of form `https://website.com/data.js`
-      if(url.startsWith("http://") || url.startsWith("https://")) return protocol + window.location.host + "/" + url;
+      if (url.startsWith("http://") || url.startsWith("https://"))
+        return protocol + window.location.host + "/" + url;
       // Urls of form `/index.js`
-      if(url.charAt(0) === "/"){
-        return (protocol + window.location.host + "/" + hostName + url);
+      if (url.charAt(0) === "/") {
+        return protocol + window.location.host + "/" + hostName + url;
       }
       //Load from relative path
-      return (protocol + window.location.host + "/" + hostName + "/" + url);
-    }
-    function reloadAllElements(){
+      return protocol + window.location.host + "/" + hostName + "/" + url;
+    };
+    function reloadAllElements() {
       //List of elements to remake
-      const remakeList = ["img", "a", "form", "iframe", "frame", "source", "object"];
-      function reloadElements(elemName){
+      const remakeList = [
+        "img",
+        "a",
+        "form",
+        "iframe",
+        "frame",
+        "source",
+        "object"
+      ];
+      function reloadElements(elemName) {
         const scriptList = Array.from(document.getElementsByTagName(elemName));
-        
-        const makeScript = function(src, link){
+
+        const makeScript = function(src, link) {
           const script = document.createElement(elemName);
           script.dataset.used = "true";
-          
-          if(elemName === "script"){
+
+          if (elemName === "script") {
             script.src = src;
-          }else if(elemName === "link"){
+          } else if (elemName === "link") {
             script.href = src;
             script.rel = link.rel;
-            if(link.getAttribute("as")) script.as = link.getAttribute("as");        
+            if (link.getAttribute("as")) script.as = link.getAttribute("as");
           }
           document.head.appendChild(script);
-        }
-        const remakeElem = (elem) => {
-          const cleanedProps = ["src", "data-original", "href", "action", "codebase"];
-          cleanedProps.forEach((prop) => elem[prop] && (elem[prop] = cleanUrl(elem[prop])));
-          
-          if(elem["srcset"]){
+        };
+        const remakeElem = elem => {
+          const cleanedProps = [
+            "src",
+            "data-original",
+            "href",
+            "action",
+            "codebase"
+          ];
+          cleanedProps.forEach(
+            prop => elem[prop] && (elem[prop] = cleanUrl(elem[prop]))
+          );
+
+          if (elem["srcset"]) {
             const srcsetArr = elem["srcset"].split(" ");
             //Hack
-            elem["srcset"] = srcsetArr.map((part) => part.includes("/")? cleanUrl(part): part).join(" ");
+            elem["srcset"] = srcsetArr
+              .map(part => (part.includes("/") ? cleanUrl(part) : part))
+              .join(" ");
           }
-        }
-        scriptList.forEach((script) => {
-          if(script.dataset.used === "true") return;
+        };
+        scriptList.forEach(script => {
+          if (script.dataset.used === "true") return;
           script.dataset.used = "true";
-          
-          const url = script.src || script.href || script.action || script.srcset;
-          if(url !== undefined && url !== ""){
+
+          const url =
+            script.src || script.href || script.action || script.srcset;
+          if (url !== undefined && url !== "") {
             const cleanedUrl = cleanUrl(url);
             console.debug(`Loading ${cleanedUrl}`);
-            if(remakeList.includes(elemName)){
+            if (remakeList.includes(elemName)) {
               remakeElem(script);
-            }else{
+            } else {
               makeScript(cleanedUrl, script);
             }
-          }else{
-            if(elemName === "script"){
-              const newScriptTag = document.createElement('script');
+          } else {
+            if (elemName === "script") {
+              const newScriptTag = document.createElement("script");
               newScriptTag.dataset.used = "true";
               newScriptTag.innerHTML = script.innerHTML;
-              document.head.appendChild(newScriptTag);          
+              document.head.appendChild(newScriptTag);
             }
           }
         });
       }
-      
+
       reloadElements("script");
       reloadElements("link");
-      
+
       reloadElements("source");
       reloadElements("iframe");
       reloadElements("object");
@@ -115,11 +141,11 @@ if(!window.injectedScriptRunOnce){
 
     XMLHttpRequest.prototype.realOpen = XMLHttpRequest.prototype.open;
 
-    var myOpen = function(method, url, async=true, user, password) {
+    var myOpen = function(method, url, async = true, user, password) {
       console.log(url, cleanUrl(url));
       //call original
-      this.realOpen (method, cleanUrl(url), async, user, password);
-    }  
+      this.realOpen(method, cleanUrl(url), async, user, password);
+    };
     //ensure all XMLHttpRequests use our custom open method
     XMLHttpRequest.prototype.open = myOpen;
   })();
