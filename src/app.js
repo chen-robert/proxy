e2426287295e9a019b70fd2c0643936f44a6a4f9
Robert Chen <robertchen@live.com>
2019-02-24 23:02:01 -0800

    Updated security

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const PORT = process.env.PORT || 3000;
 
 const compression = require("compression");
+const crypto = require("crypto");
 const express = require("express");
 const request = require("request").defaults({ jar: true });
 
@@ -11,7 +12,21 @@ const app = express();
 app.enable("trust proxy");
 app.use(util.dataParser);
 app.use(compression());
+app.use(require("cookie-parser")());
 app.use(util.forceSSL);
+
+app.get("/auth/:token", (req, res) => {
+  if(req.params.token === "proxy") res.cookie("authid", crypto.randomBytes(40).toString('hex'), { maxAge: 365 * 24 * 60 * 60 * 1000, httpOnly: true});
+  
+  res.redirect("/");
+});
+
+app.use((req, res, next) => {
+  if(!req.cookies.authid){
+    return res.redirect("https://bsd405.org");
+  }
+  next();
+});
 
 app.get("/encrypt.js", (req, res) => {
   res.sendFile(`${__dirname}/encrypt.js`);
