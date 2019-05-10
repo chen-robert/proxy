81c569505d9d2b17f89959a0103e4167c7ddb5ae
Robert <robertchen@live.com>
2019-05-10 20:08:57 +0000

    Reverted injected script changes

--- src/client/inject.js
+++ src/client/inject.js
@@ -130,6 +130,21 @@ if (!window.injectedScriptRunOnce) {
     // ensure all XMLHttpRequests use our custom open method
     XMLHttpRequest.prototype.open = myOpen;
 
+    const remakeHTMLFn = fnName => {
+      const oriFn = HTMLElement.prototype[fnName];
+      HTMLElement.prototype[fnName] = function() {
+        arguments = Array.from(arguments).map(elem => {
+          if (elem instanceof HTMLElement && elem.dataset.used !== "true") {
+            remakeElem(elem);
+          }
+          return elem;
+        });
+        oriFn.call(this, ...arguments);
+      };
+    };
+
+    ["appendChild", "insertBefore"].forEach(remakeHTMLFn);
+
     window._history = {
       pushState: (...args) => console.log(args)
     };
