24a805070d7d8b1716ac51605ff6ee8245853735
Robert <robertchen@live.com>
2019-05-26 18:00:25 +0000

    Updates to frameforwarding

--- src/encrypt.js
+++ src/encrypt.js
@@ -12,13 +12,15 @@
     });
   }
   const MAC = "AUTH";
+  const ivLen = 8;
   const getKey = secret => aesjs.utils.hex.toBytes(secret || fallback);
   exports.encode = (text, secret) => {
     const key = getKey(secret);
     const aesCtr = new aesjs.ModeOfOperation.ctr(key);
+    const iv = () => ("" + Math.random() + "A".repeat(ivLen)).substring(2).substring(0, ivLen);
     return text
       .split("/")
-      .map(elem => aesjs.utils.hex.fromBytes(aesCtr.encrypt(aesjs.utils.utf8.toBytes(MAC + elem))))
+      .map(elem => aesjs.utils.hex.fromBytes(aesCtr.encrypt(aesjs.utils.utf8.toBytes(iv() + MAC + elem))))
       .join("/");
   }
   exports.decode = (cipher, secret) => {
@@ -27,7 +29,7 @@
     return cipher
       .split("/")
       .map(elem => {
-        const decoded = aesjs.utils.utf8.fromBytes(aesCtr.decrypt(aesjs.utils.hex.toBytes(elem)))
+        const decoded = aesjs.utils.utf8.fromBytes(aesCtr.decrypt(aesjs.utils.hex.toBytes(elem))).substring(ivLen)
         if(decoded.substring(0, MAC.length) !== MAC) {
           throw "Invalid MAC"; 
         }
